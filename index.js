841a57b36aa4f42b753058f8c272789652d56ecd
bhavana861 <bhavanarajendran23@gmail.com>
2024-11-17 15:13:09 +0530

    message

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const jsonServer = require('json-server')
 
 const WTServer = jsonServer.create()
 const middleware = jsonServer.defaults()
-const PORT = 3000
+const PORT = process.env.PORT || 3000  // Use environment variable for PORT or fallback to 3000
 
 const route = jsonServer.router('db.json')
 
 WTServer.use(middleware)
 WTServer.use(route)
 
-WTServer.listen(PORT,()=>{
-    console.log(`WTServer started at PORT : ${PORT} and waiting for client request!!!`);  
+WTServer.listen(PORT, () => {
+    console.log(`WTServer started at PORT : ${PORT} and waiting for client request!!!`)
 })
